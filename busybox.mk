8aa2ba306e6f05abefacd052ff79a7d31fda1238
eraselk <eraselk@proton.me>
2024-07-28 20:43:20 +0800

    Update script

--- busybox.mk
+++ busybox.mk
@@ -6,6 +6,6 @@ LOCAL_LDFLAGS := -static -Wl,--wrap=realpath -Wl,--wrap=rename -Wl,--wrap=rename
 LOCAL_CFLAGS := \
 -w -include include/autoconf.h -D__USE_BSD -D__USE_GNU \
 -DBB_VER=\"$(BB_VER)\" -DBB_BT=AUTOCONF_TIMESTAMP \
--DBB_BUILDER=\"$(BB_BUILDER)\" -DBB_NAME=\"$(BB_NAME)\" 
+-DBB_BUILDER=\"$(BB_BUILDER)\" -DBB_NAME=\"$(BB_NAME)\"
 
 LOCAL_SRC_FILES := \
