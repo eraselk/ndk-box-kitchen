a46653e2b2c8fa7ebb1cfd75d2c78e576a206493
eraselk <eraselk@proton.me>
2024-08-09 11:13:40 +0800

    Minor changes

--- busybox.mk
+++ busybox.mk
@@ -5,7 +5,7 @@ LOCAL_STATIC_LIBRARIES := libselinux
 LOCAL_LDFLAGS := -static -Wl,--wrap=realpath -Wl,--wrap=rename -Wl,--wrap=renameat
 LOCAL_CFLAGS := \
 -w -include include/autoconf.h -D__USE_BSD -D__USE_GNU \
--DBB_VER=\"$(BB_VER)\" -DBB_BT=AUTOCONF_TIMESTAMP \
--DBB_BUILDER=\"$(BB_BUILDER)\" -DBB_NAME=\"$(BB_NAME)\"
+-DBB_VER=\"$(BB_VER)\" -DBB_TIME_STAMP=\"$(BB_TIME_STAMP)\" \
+-DBB_NAME=\"$(BB_NAME)\" -DBB_BUILD_TYPE=\"$(BUILD_TYPE)\"
 
 LOCAL_SRC_FILES := \
